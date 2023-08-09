c7b631a1eec54ae5c16935dd71702cb1d4986ab3
Григорий Казаков <gresha300699@yandex.ru>
2023-08-09 21:01:19 +0700

    version_4

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
-MEDIA_URL = '/app/media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
