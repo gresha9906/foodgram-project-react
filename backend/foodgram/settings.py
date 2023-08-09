55bad29770b37ccc8d7703832c9cb7066291a416
Григорий Казаков <gresha300699@yandex.ru>
2023-08-09 16:57:45 +0700

    version_3

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -72,7 +72,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
+        'HOST': os.getenv('DB_HOST', ''),
         'PORT': '5432',
     }
 }
